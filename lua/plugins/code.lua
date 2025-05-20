6794bed08e1423eafaf9a5c866cf0ca1a3625b13
macductan <tanmd@foobla.com>
2025-05-20 15:20:44 +0700

    Refactor vim-sleuth autocommand to include BufRead event and add condition for setting tabstop

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -3,12 +3,14 @@ return {
   { "tpope/vim-sleuth",
     config = function()
       -- Set tabstop=2 for new files
-      vim.api.nvim_create_autocmd("BufNewFile", {
+      vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
         pattern = "*",
         callback = function()
-          vim.bo.tabstop = 2
-          vim.bo.shiftwidth = 2
-          vim.bo.softtabstop = 2
+          if vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
+            vim.bo.tabstop = 2
+            vim.bo.shiftwidth = 2
+            vim.bo.softtabstop = 2
+          end
         end,
       })
     end 
