b972f8954c3dbe4a69657faa02bd20666d2fac29
macductan <tanmd@foobla.com>
2025-05-20 15:17:30 +0700

    Add configuration for vim-sleuth to automatically set tabstop and shiftwidth

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,4 +1,16 @@
 return {
   -- Automatically set tabstop and shiftwidth
-  { "tpope/vim-sleuth" },
+  { "tpope/vim-sleuth",
+    config = function()
+      -- Set tabstop=2 for new files
+      vim.api.nvim_create_autocmd("BufNewFile", {
+        pattern = "*",
+        callback = function()
+          vim.bo.tabstop = 2
+          vim.bo.shiftwidth = 2
+          vim.bo.softtabstop = 2
+        end,
+      })
+    end 
+  },
 }
