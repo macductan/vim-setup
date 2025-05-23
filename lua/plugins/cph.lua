af144373e3e0f1f541d3bd08674120737aeceed1
macductan <tanmd@foobla.com>
2025-05-23 15:45:04 +0700

    Add winbar option and key mappings for cphelper.nvim commands

--- lua/plugins/cph.lua
+++ lua/plugins/cph.lua
@@ -6,5 +6,50 @@ return {
     config = function()
       vim.g["cph#lang"] = "python"
     end,
+    keys = {
+      {
+        "<leader>cpr",
+        function()
+          if vim.bo.filetype == "python" then
+            vim.cmd(":CphReceive")
+          else
+            vim.notify("This command is only available in Python files", vim.log.levels.WARN)
+          end
+        end,
+        desc = "CphReceive",
+      },
+      {
+        "<leader>cpt",
+        function()
+          if vim.bo.filetype == "python" then
+            local number = vim.fn.input("Test number: ")
+            if number ~= "" then
+              vim.cmd(":CphTest " .. number)
+            else
+              vim.cmd(":CphTest")
+            end
+          else
+            vim.notify("This command is only available in Python files", vim.log.levels.WARN)
+          end
+        end,
+        desc = "CphTest",
+      },
+      {
+        "<leader>cpe",
+        function()
+          if vim.bo.filetype == "python" then
+            local number = vim.fn.input("Edit number: ")
+            if number ~= "" then
+              vim.cmd(":CphEdit " .. number)
+            else
+              vim.cmd(":CphEdit")
+            end
+          else
+            vim.notify("This command is only available in Python files", vim.log.levels.WARN)
+          end
+        end,
+        desc = "CphEdit",
+      },
+    }
   },
 }
