cdac80ab59bd32dea27a829958b9c5b757c84d1a
macductan <tanmd@foobla.com>
2025-06-06 17:16:57 +0700

    Add autocmd to set Neovim window title based on first argument

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,3 +22,20 @@ vim.g.copilot_filetypes = {
 vim.api.nvim_set_keymap("t", "<C-w>", [[<C-\><C-n><C-w>]], { noremap = true, silent = true })
 
 vim.opt.wrap = true
+
+-- Set the title of the Neovim window based on the first non-option argument
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.opt.title = true
+    local args = vim.v.argv
+    local title = "nvim"
+    for i = 2, #args do
+      local arg = args[i]
+      if not arg:match("^%-") then
+        title = "nvim " .. vim.fn.fnamemodify(arg, ":p")
+        break
+      end
+    end
+    vim.opt.titlestring = title
+  end,
+})
